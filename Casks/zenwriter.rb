a6a6a80ab9a1ff86213209067afc1dc435c73da6
ccastellanos_trevipay <ccastellanos@trevipay.com>
2025-07-28 10:20:13 -0500

    fix zenwriter cask

--- Casks/zenwriter.rb
+++ Casks/zenwriter.rb
@@ -2,8 +2,8 @@ cask "zenwriter" do
     version "0.0.1-beta"
     sha256 "6e0c1aab29073d686b4adc13b1a36323b676d393b78bcbfbdb6851866bd84103" 
   
-   url "https://objects.githubusercontent.com/github-production-release-asset-XYZ/ZenWriter-0.0.1-arm64.dmg",
-    verified: "githubusercontent.com"
+    url "https://github.com/cristianjoan/zenwriter/releases/download/0.0.1-beta/ZenWriter-0.0.1-arm64.dmg",
+        verified: "github.com/cristianjoan/zenWriter"
   
     name "ZenWriter"
     desc "Minimalist writing app with Markdown support"
